8fc9408fd7d201551a401b515b1f42e37b65f963
heaven-wanderer <luckystqx@gmail.com>
2024-03-22 22:48:09 +0300

    Прогноз на несколько дней -Реализован прогноз погоды на несколько дней -Добавлен колбэк -Немного изменена структура

---
+++ keyboards/inline.py
@@ -0,0 +1,16 @@
+from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
+
+inl_kb = InlineKeyboardMarkup(
+    inline_keyboard=[
+        [
+            InlineKeyboardButton(text = "2", callback_data="2"),
+            InlineKeyboardButton(text = "3", callback_data="3"),
+            InlineKeyboardButton(text = "4", callback_data="4")
+        ],
+        [
+            InlineKeyboardButton(text = "5", callback_data="5"),
+            InlineKeyboardButton(text = "6", callback_data="6"),
+            InlineKeyboardButton(text = "7", callback_data="7")
+        ]
+    ]
+)
---
+++ logic/weather.py
@@ -0,0 +1,95 @@
+import requests
+import json
+
+from datetime import datetime,timedelta
+from config_reader import config
+from datetime import datetime
+
+async def connect_yandex():
+    url_yandex = f'https://api.weather.yandex.ru/v2/forecast?lat=57.76791&lon=40.926894&[lang=ru_RU]'
+    yandex_req = requests.get(url_yandex, headers={'X-Yandex-API-Key': config.yandex_api.get_secret_value()})
+    data = json.loads(yandex_req.text)
+    return data
+
+async def get_weather():
+    data = await connect_yandex()
+
+    temp = data['fact']['temp']
+    humidity = data['fact']['humidity']
+    feels_like = data['fact']['feels_like']
+    wind_speed = data['fact']['wind_speed']
+    condition = data['fact']['condition']
+    pressure_mm = data['fact']['pressure_mm']
+    current_condition = conditions.get(condition)
+
+    return {
+        'date': (datetime.now()).strftime('%Y-%m-%d'),
+        'temp': temp,
+        'humidity': humidity,
+        'feels_like': feels_like,
+        'wind_speed': wind_speed,
+        'pressure_mm': pressure_mm,
+        'condition': current_condition
+    }
+
+async def get_weatherForecast(forecast_dates):
+    data = await connect_yandex()
+    
+    forecasts = []
+    for forecast_date in forecast_dates:
+        forecast = next(filter(lambda x: x['date'] == forecast_date, data['forecasts']), None)
+        if forecast:
+            day_short_forecast = forecast['parts']['day']
+            temp = day_short_forecast['temp_avg']
+            humidity = day_short_forecast['humidity']
+            feels_like = day_short_forecast['feels_like']
+            wind_speed = day_short_forecast['wind_speed']
+            condition = day_short_forecast['condition']
+            pressure_mm = day_short_forecast['pressure_mm']
+            current_condition = conditions.get(condition)
+            forecasts.append({
+                'date': forecast_date,
+                'temp': temp,
+                'humidity': humidity,
+                'feels_like': feels_like,
+                'wind_speed': wind_speed,
+                'pressure_mm': pressure_mm,
+                'condition': current_condition
+            })
+    
+    return forecasts
+
+async def textWeather(forecast_dates=None):
+    if forecast_dates:
+        forecasts = await get_weatherForecast(forecast_dates)
+    else:
+        forecasts = [await get_weather()]
+
+    weather_message = "Прогноз погоды в городе Кострома:\n\n"
+    for forecast in forecasts:
+        weather_message += f"Дата: {forecast['date']}\n"
+        weather_message += f"Температура: {forecast['temp']}°C (ощущается как {forecast['feels_like']}°C)\n"
+        weather_message += f"Скорость ветра: {forecast['wind_speed']} м/c\n"
+        weather_message += f"Влажность: {forecast['humidity']}%\n"
+        weather_message += f"Давление: {forecast['pressure_mm']} (в мм рт.ст.)\n"
+        weather_message += f"Состояние погоды: {forecast['condition']}\n\n"
+    return weather_message
+
+async def listDate(dat):
+    start_date = datetime.now()
+    buttons = []
+    for i in range(dat):
+        date = start_date + timedelta(days=i)
+        formatted_date = date.strftime('%Y-%m-%d')
+        buttons.append(formatted_date)
+    return buttons
+    
+
+conditions = {'clear': 'ясно', 'partly-cloudy': 'малооблачно', 'cloudy': 'облачно с прояснениями',
+                  'overcast': 'пасмурно', 'drizzle': 'морось', 'light-rain': 'небольшой дождь',
+                  'rain': 'дождь', 'moderate-rain': 'умеренно сильный', 'heavy-rain': 'сильный дождь',
+                  'continuous-heavy-rain': 'длительный сильный дождь', 'showers': 'ливень',
+                  'wet-snow': 'дождь со снегом', 'light-snow': 'небольшой снег', 'snow': 'снег',
+                  'snow-showers': 'снегопад', 'hail': 'град', 'thunderstorm': 'гроза',
+                  'thunderstorm-with-rain': 'дождь с грозой', 'thunderstorm-with-hail': 'гроза с градом'
+             }
